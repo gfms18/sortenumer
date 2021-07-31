07a1bd629b10ae0e94caf171fbd182ccd753171a
Gabriel Felipe <72725178+gfms18@users.noreply.github.com>
2021-07-31 16:04:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,9 +6,24 @@ function getRandomArbitrary(min, max) {
 function apertar(){
     var valorminimo = parseInt($("#valorminimo").val());
     var valormaximo = parseInt($("#valormaximo").val());
-    valoraleatoriogerado = getRandomArbitrary(valorminimo, valormaximo);
-    $("#valordosorteio").text(valoraleatoriogerado)
-    console.log(valoraleatoriogerado);
+    if (valorminimo < valormaximo){
+        valoraleatoriogerado = getRandomArbitrary(valorminimo, valormaximo);
+        $("#valordosorteio").text(valoraleatoriogerado)
+        console.log(valoraleatoriogerado);
+    } 
+    if(valorminimo == valormaximo){
+        $("#valordosorteio").text("erro")
+        alert("valor minimo é igual ao valor maximo")
+    }
+
+
+    if(valorminimo > valormaximo) {
+        $("#valordosorteio").text('erro')
+        alert("valor minimo é maior que valor maximo")
+        
+    }
+
+    
 
 };
 
